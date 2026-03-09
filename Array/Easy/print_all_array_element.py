f4870ef6a1820c4b61a07b16675397c464597d18
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-09 08:43:40 +0530

    print_all_array_element

---
+++ Array/Easy/print_all_array_element.py
@@ -0,0 +1,6 @@
+# Write a Python program to traverse an array and print all its elements one by one.
+# The program should read the array and display each element on a new line.
+
+arr = [5, 2, 9, 1, 7]
+for i in arr:
+    print(i)

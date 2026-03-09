2ef6eae0be85f4ba0df7e6b46a9b30a43d506680
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-09 08:51:56 +0530

    sum of all array elements

---
+++ Array/Easy/sum_of_all_array_element.py
@@ -0,0 +1,14 @@
+
+arr = [3, 6, 2, 8, 4]
+
+# first and easy approch to directally get the sum 
+# print(sum(arr))
+
+# second and treditional way to get sum of all element 
+
+total = 0
+
+for num in arr:
+    total += num
+
+print("Sum of array:", total)

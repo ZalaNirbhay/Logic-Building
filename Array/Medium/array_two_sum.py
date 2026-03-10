919ed5872f609216178b3592d137672ba8140251
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-03-10 16:09:47 +0530

    move all zero to end in array

--- Array/Medium/array_two_sum.py
+++ Array/Medium/array_two_sum.py
@@ -4,7 +4,7 @@
 def two_sum(arr,target):
     elems=[]
     for i in range(len(arr)):
-        for j in range(len(arr)):
+        for j in range(i+1,len(arr)):
             if arr[i]+arr[j]==target:
                 return i,j
 
---
+++ Array/Medium/move_zeo_to_end.py
@@ -0,0 +1,22 @@
+# Write a Python program to move all zeros in an array to the end while maintaining the #order of non-zero elements.
+
+def move_zero_end(arr):
+    i,j=0,1
+    if (arr[j]!=0 and arr[i]==0):
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+
+    while(True):
+        if (i==len(arr) or j==len(arr)):
+            return arr
+        if (arr[j]==0):
+            j += 1
+        if (arr[j] != 0):
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j += 1
+        if i == j:
+            j += 1
+                
+arr = [1,0,0,3,12]
+print(move_zero_end(arr))

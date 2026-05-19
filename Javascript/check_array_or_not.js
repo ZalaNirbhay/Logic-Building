058a19715de0a4aa4a16ff74af6bc3f22c354dfc
zalanirbhay21@gmail.com <zalanirbhay21@gmail.com>
2026-05-19 21:21:17 +0530

    add functions to check if an element is an array and print odd terms from an array

---
+++ Javascript/check_array_or_not.js
@@ -0,0 +1,6 @@
+function checkArray(elem){
+    return Array.isArray(elem)
+}
+
+console.log([]);
+console.log([]);
---
+++ Javascript/print_odd_terms.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arr.length - 2; i += 2) {
+  console.log(arr[i]);
+}
+for (let j = arr.length - 2; j < arr.length; j++) {
+  console.log(arr[j]);
+}
